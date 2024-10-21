3ce29effcb187167f9cea2f7e96211b54cfad82a
Jeffrey-Erbach <jeffrey.erbach12293@outlook.com>
2024-10-21 13:59:21 +0200

    Hausaufgabe 21-10-24

---
+++ DOM+node/first-react-app/hausaufgabe-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  // Definiere eine Variable für den aktuellen Tag oder eine Begrüßung
+  const greeting = "Heute ist ein großartiger Tag!";
+
+  return (
+    <>
+      <h1>Willkommen zu meiner ersten React-App</h1>
+      <h2>{greeting}</h2>
+      <h3>2 + 3 = {2 + 3}</h3>
+    </>
+  );
+}
+
+export default App;
